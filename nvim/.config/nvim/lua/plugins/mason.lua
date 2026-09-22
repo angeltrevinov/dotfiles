ed26cd4cd3430993b9b9a7d2f2afce38d8260cd2
angeltrevinov <angeltrevinov@users.noreply.github.com>
2026-09-21 19:46:43 -0600

    Merge mason.lua fixes: lspconfig split + name annotation
    Uncommitted repo edit moved LSP servers into mason-lspconfig.nvim
    (correct, lspconfig names); live edit annotated that mason.nvim's
    ensure_installed uses mason package names. Live had no LSP auto-install
    at all. Merged both so fresh installs get LSP servers again.

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -3,16 +3,23 @@ return {
     "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
+        -- Formatters / linters (mason package names)
+        "stylua",
+        "ruff",
+        "prettierd",
+        "shfmt",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
         -- LSP servers
         "lua_ls",
         "basedpyright",
         "bashls",
         "ts_ls",
-        -- Formatters / linters
-        "stylua",
-        "ruff",
-        "prettierd",
-        "shfmt",
       },
     },
   },
